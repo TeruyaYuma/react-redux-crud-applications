03bbe509e220d524e35b0e8c1b14f11643497fd7
照屋　勇真 <ky19860120@gmail.com>
2020-11-27 02:20:55 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ function App() {
     <React.Fragment>
     <label htmlFor="bar">bar</label>
     <input type="text" onClick={ () => {console.log('click')} }/>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
     </React.Fragment>
     
   );
+}
+
+function Cat() {
+  return <div>cat</div>;
 }
 
 export default App;
